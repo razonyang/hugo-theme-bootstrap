bdb9bd041649db2a49dc206a92a656b368219a40
razonyang <razonyang@gmail.com>
2020-10-19 02:07:32 +0800

    Update scroll top icon

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,11 @@ import './table'
 import './scroll-top'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faArrowRight, faCalendarAlt, faChevronUp, faEnvelope, faFolder, faLanguage, faTag } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faTag } from '@fortawesome/free-solid-svg-icons'
 import { faFacebook, faGithub, faGitlab, faLinkedin, faStackOverflow, faTwitter, faWeibo, faZhihu } from '@fortawesome/free-brands-svg-icons'
 
 // Add solid icons.
-library.add(faArrowRight, faCalendarAlt, faChevronUp, faEnvelope, faFolder, faLanguage, faTag)
+library.add(faArrowRight, faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faTag)
 
 // Add brand icons.
 library.add(faFacebook, faGithub, faGitlab, faLinkedin, faStackOverflow, faTwitter, faWeibo, faZhihu)
