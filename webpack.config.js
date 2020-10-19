0ac2e57ac14dfdc69ee8a7cc9ac2db64459d5e20
razonyang <razonyang@gmail.com>
2020-10-19 20:41:34 +0800

    Update webpack.config.js and rebuild assets

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,9 @@ module.exports = {
       new copyPlugin({
         patterns: [
           { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: basePath+'/static/css/bootstrap.min.css' },
+          { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css.map', to: basePath+'/static/css/bootstrap.min.css.map' },
           { from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', to: basePath+'/static/js/bootstrap.bundle.min.js' },
+          { from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', to: basePath+'/static/js/bootstrap.bundle.min.js.map' },
           { from: 'node_modules/@fortawesome/fontawesome-free/css/all.min.css', to: basePath+'/static/css/fontawesome.all.min.css' },
           { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: basePath+'/static/webfonts' },
         ],
