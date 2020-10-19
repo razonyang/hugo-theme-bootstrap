ec2d265343d9c6baba6c167a46ce7a932b058cf5
razonyang <razonyang@gmail.com>
2020-10-20 00:42:49 +0800

    Update JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,27 @@
-import './table'
-import './scroll-top'
+document.addEventListener('DOMContentLoaded', function() {
+  scrollTop()
+  initTableClass()
+})
+
+function scrollTop() {
+  let scrollTop = document.getElementById('scrollTop')
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollTop.style.display = "block";
+    } else {
+      scrollTop.style.display = "none";
+    }
+  };
+
+  scrollTop.onclick = function () {    
+    document.documentElement.scrollTop = 0;
+  }
+}
+
+function initTableClass() {
+  let tables = document.getElementsByTagName('table')
+  let className = 'table table-bordered table-striped {{ .Site.Params.dateFormat }}'
+  for (let i=0; i < tables.length; i++) {
+    tables[i].setAttribute('class', className)
+  }
+}
--- assets/js/table.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let tables = document.getElementsByTagName('table')
-  let className = 'table table-bordered table-striped'
-  for (let i=0; i < tables.length; i++) {
-    tables[i].setAttribute('class', className)
-  }
-})
