8bfaedbd0a21758b17cc5becdb3026c274724dde
razonyang <razonyang@gmail.com>
2020-10-28 16:44:40 +0800

    Add Quora and Instagram social links

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -3,11 +3,11 @@ import {
   faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faSearch, faSun, faTag,
 } from '@fortawesome/free-solid-svg-icons'
 import {
-  faFacebook, faGithub, faGitlab, faLinkedin, faStackOverflow, faTwitter, faWeibo, faZhihu
+  faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu,
 } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faSearch, faSun, faTag)
-library.add(faFacebook, faGithub, faGitlab, faLinkedin, faStackOverflow, faTwitter, faWeibo, faZhihu)
+library.add(faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu)
 
 document.addEventListener('DOMContentLoaded', function() {
   dom.watch()
