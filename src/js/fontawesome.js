0ed370aa8fff03d5b7e4964882f6ac1249c91b6b
razonyang <razonyang@gmail.com>
2020-10-30 20:08:57 +0800

    Add tumblr social link

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -4,10 +4,14 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import {
   faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu,
+  faTumblr,
 } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faSearch, faSun, faTag)
-library.add(faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu)
+library.add(
+  faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu,
+  faTumblr,
+)
 
 document.addEventListener('DOMContentLoaded', function() {
   dom.watch()
