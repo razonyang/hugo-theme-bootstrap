df688e02fd6793cfd7c558307981af4dd95f8785
razonyang <razonyang@gmail.com>
2020-11-02 14:20:12 +0800

    Add palette icon

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,13 +1,13 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import {
-  faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faSearch, faSun, faTag,
+  faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faPalette, faSearch, faSun, faTag,
 } from '@fortawesome/free-solid-svg-icons'
 import {
   faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu,
   faTumblr,
 } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faSearch, faSun, faTag)
+library.add(faCalendarAlt, faChevronCircleUp, faEnvelope, faFolder, faLanguage, faMoon, faPalette, faSearch, faSun, faTag)
 library.add(
   faFacebook, faGithub, faGitlab, faInstagram, faLinkedin, faQuora, faStackOverflow, faTwitter, faWeibo, faZhihu,
   faTumblr,
