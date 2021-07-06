34913bb99c199efc83e6b60a8ba6e6fddba01028
razonyang <razonyang@gmail.com>
2021-07-07 00:08:38 +0800

    Update the sidebar toggler and TOC

--- src/js/sidebar-toggler.ts
+++ src/js/sidebar-toggler.ts
@@ -2,6 +2,7 @@ class SidebarToggler {
     btn: HTMLElement;
     main: HTMLElement;
     sidebar: HTMLElement;
+    key: string = 'hbs-sidebar-toggler'
 
     constructor(public button: HTMLElement) {
         this.btn = button;
@@ -14,24 +15,36 @@ class SidebarToggler {
         this.btn.addEventListener('click', function () {
             self.toggle();
         })
+
+        
+        const val = localStorage.getItem(this.key);
+        if (val === 'hide' && this.isShown()) {
+            this.hide()
+        }
     }
 
     toggle() {
-        if (this.main.classList.contains('col-lg-8')) {
+        if (this.isShown()) {
             this.hide()
         } else {
             this.show()
         }
     }
 
+    isShown() {
+        return this.main.classList.contains('col-lg-8')
+    }
+
     hide() {
         this.main.classList.replace('col-lg-8', 'col-lg-12')
         this.sidebar.classList.add('d-none')
+        localStorage.setItem(this.key, 'hide')
     }
 
     show() {
         this.main.classList.replace('col-lg-12', 'col-lg-8')
         this.sidebar.classList.remove('d-none')
+        localStorage.removeItem(this.key)
     }
 }
 
