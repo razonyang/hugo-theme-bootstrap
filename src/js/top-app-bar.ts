06cb645a1d06f0dc3cd2a34d5544b92479a7333b
razonyang <razonyang@gmail.com>
2021-12-07 12:55:25 +0800

    feat: add the copy URL button

--- src/js/top-app-bar.ts
+++ src/js/top-app-bar.ts
@@ -1,7 +1,13 @@
+import GlobalSnackbar from "./snackbar";
+
 class TopAppBar {
     static run() {
-        document.querySelector('#pageReloader').addEventListener('click', function() {
+        document.querySelector('.action-reload-page').addEventListener('click', function() {
             window.location.reload();
+        });
+        document.querySelector('.action-copy-url').addEventListener('click', function() {
+            navigator.clipboard.writeText(window.location.href);
+            GlobalSnackbar.show('Copied');
         });
     }
 }
