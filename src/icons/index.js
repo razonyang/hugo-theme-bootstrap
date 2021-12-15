750d49214105a3ed891db62a96b56d02248f68d2
razonyang <razonyang@gmail.com>
2021-12-15 09:17:01 +0800

    feat: add the comment action to the post panel

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,7 +1,7 @@
 import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
 import {
   faAdjust, faArrowsAltV, faArrowLeft, faBars, faBuilding, faCheckCircle, faChevronCircleDown,
-  faChevronCircleUp, faChevronLeft, faChevronRight, faChevronUp, faCode, faCoffee,
+  faChevronCircleUp, faChevronLeft, faChevronRight, faChevronUp, faCode, faCoffee, faComments,
   faColumns, faCopy, faCopyright, faEdit, faEllipsisV, faEnvelope, faExclamationTriangle,
   faExpandAlt, faEyeDropper, faFont, faFileAlt, faFileArchive, faFolder, faHome, faInfoCircle,
   faLanguage, faLink, faList, faListAlt, faMapMarkerAlt, faPalette, faQuestion, faQuestionCircle,
@@ -25,7 +25,7 @@ config.searchPseudoElements = true;
 library.add(
   // Solid Icons
   faAdjust, faArrowsAltV, faArrowLeft, faBars, faBuilding, faCheckCircle, faChevronCircleDown,
-  faChevronCircleUp, faChevronLeft, faChevronRight, faChevronUp, faCode, faCoffee,
+  faChevronCircleUp, faChevronLeft, faChevronRight, faChevronUp, faCode, faCoffee, faComments,
   faColumns, faCopy, faCopyright, faEdit, faEllipsisV, faEnvelope, faExclamationTriangle,
   faExpandAlt, faEyeDropper, faFont, faFileAlt, faFileArchive, faFolder, faHome, faInfoCircle,
   faLanguage, faLink, faList, faListAlt, faMapMarkerAlt, faPalette, faQuestion, faQuestionCircle,
