f0aea04af2c4e227131575f40744a0aeb57b809d
razonyang <razonyang@gmail.com>
2022-02-22 23:29:57 +0800

    chore: remove exampleSite/src

--- exampleSite/src/icons/index.js
+++
@@ -1,8 +0,0 @@
-// import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
-// import { faGlobe, faClock } from '@fortawesome/free-solid-svg-icons';
-
-// config.searchPseudoElements = true;
-
-// library.add(faGlobe, faClock); // Add icons that you need.
-
-// dom.watch();
