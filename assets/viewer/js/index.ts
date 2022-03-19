74a576817d9a5a37515457f4770bddc81c18569e
razonyang <razonyang@gmail.com>
2022-03-20 00:35:25 +0800

    feat(image-viewer): add the data-viewer-invisible attribute for image tag
    Images with the data-viewer-invisible attribute will be filtered by the image viewer.

--- assets/viewer/js/index.ts
+++ assets/viewer/js/index.ts
@@ -3,16 +3,12 @@ import options from './options';
 
 class Gallery {
     gallery: Viewer;
-    
-    excludeClassNames = [
-      'profile-avatar',
-    ];
 
     constructor() {
         const self = this;
         const defaultOptions: Viewer.Options = {
             filter(img: HTMLImageElement) {
-                return self.isImageValid(img);
+                return self.validate(img);
             },
             url(img: HTMLImageElement) {
               return img.hasAttribute('data-src') ? img.getAttribute('data-src') : img.src;
@@ -25,7 +21,7 @@ class Gallery {
     run() {
         const self = this;
         document.querySelectorAll('img').forEach(function (img) {
-            if (img.parentElement.tagName !== 'A' && self.isImageValid(img)) {
+            if (img.parentElement.tagName !== 'A' && self.validate(img)) {
               img.addEventListener('click', function () {
                 self.gallery.show();
               });
@@ -33,8 +29,8 @@ class Gallery {
         });
     }
 
-    isImageValid(img) {
-      return this.excludeClassNames.filter(name => img.classList.contains(name)).length == 0;
+    validate(img) {
+      return !img.hasAttribute('data-viewer-invisible');
     }
 }
 
