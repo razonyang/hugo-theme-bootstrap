104dd052de681472d595092165e6ed9d5e1e64ef
Andy Costanza <andy.costanza@pm.me>
2022-03-27 20:40:29 +0200

    feat: add new custom icon for tipeee.com

--- assets/icons/icons/index.js
+++ assets/icons/icons/index.js
@@ -1,9 +1,11 @@
 import faBilibili from './faBilibili';
 import faLiberapay from './faLiberapay';
 import faOffline from './faOffline';
+import faTipeee from './faTipeee';
 
 export {
   faBilibili,
   faLiberapay,
   faOffline,
+  faTipeee,
 };
