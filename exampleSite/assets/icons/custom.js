ced461472acdd9db975b4c897693064d402bfba7
razonyang <razonyang@gmail.com>
2022-06-14 14:52:43 +0800

    demo: create the donate menus

--- exampleSite/assets/icons/custom.js
+++ exampleSite/assets/icons/custom.js
@@ -1,11 +1,11 @@
 import {
-    faBook, faBug, faImage, faImages, faNewspaper,
+    faBook, faBug, faHeart, faImage, faImages, faNewspaper,
 } from '@fortawesome/free-solid-svg-icons';
 // import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
 // import { faAmazon, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 const icons = [
-    faBook, faBug, faImage, faImages, faNewspaper,
+    faBook, faBug, faHeart, faImage, faImages, faNewspaper,
     // faAddressBook,
     // faAmazon, faGoogle,
 ];
