ba47d0f0bd4ad9f35c2f68446ddc556c0077cf81
razonyang <razonyang@gmail.com>
2022-07-18 19:22:10 +0800

    chore: create another postcss.config.js for RTL

--- assets/main/config/rtl/postcss.config.js
+++ assets/main/config/rtl/postcss.config.js
@@ -1,10 +1,12 @@
-const purgecss = require('./purgecss.config.js').default;
+const purgecss = require('../../../../purgecss.config.js').default;
 
 const autoprefixer = require('autoprefixer');
+const rtlcss = require('rtlcss');
 
 module.exports = {
   plugins: [
     autoprefixer,
     purgecss,
+    rtlcss,
   ]
 }
