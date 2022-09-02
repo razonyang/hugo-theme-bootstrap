f3916c4526df051068241113b3d59dbdc897cc3b
razonyang <razonyang@gmail.com>
2022-09-03 01:23:55 +0800

    style: display TOC above of the content on small and medium screens

---
+++ assets/js/toc/index.ts
@@ -0,0 +1,22 @@
+import Component from "js/component";
+
+export default class TOC implements Component
+{
+    private container: HTMLElement;
+    private target: HTMLElement;
+
+    constructor(container: string, target: string) {
+        this.container = document.querySelector(container);
+        this.target = document.querySelector(target);
+    }
+
+    run() {
+        if (!this.target || !this.container) {
+            return;
+        }
+        
+        const child = this.target.cloneNode(true);
+        child.removeAttribute('id');
+        this.container.appendChild(child);
+    }
+}
