8b79290a834da75c27db5cb51c66d3fb5f8275d9
razonyang <razonyang@gmail.com>
2022-10-31 06:12:33 +0800

    docs: fix Mermaid examples

--- exampleSite/assets/icons/custom.js
+++ exampleSite/assets/icons/custom.js
@@ -1,11 +1,11 @@
 import {
-    faArchive, faBlog, faBook, faBug, faCloudUploadAlt, faCubes, faHandshake, faHeart, faImage, faImages, faLightbulb, faNewspaper, faSync, faTerminal,
+    faArchive, faBlog, faBook, faBug, faCar, faCloudUploadAlt, faCubes, faHandshake, faHeart, faImage, faImages, faLightbulb, faNewspaper, faSync, faTerminal
 } from '@fortawesome/free-solid-svg-icons';
 // import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
 import { faMarkdown } from '@fortawesome/free-brands-svg-icons';
 
 const icons = [
-    faArchive, faBlog, faBook, faBug, faCloudUploadAlt, faCubes, faHandshake, faHeart, faImage, faImages, faLightbulb, faNewspaper, faSync, faTerminal,
+    faArchive, faBlog, faBook, faBug, faCar, faCloudUploadAlt, faCubes, faHandshake, faHeart, faImage, faImages, faLightbulb, faNewspaper, faSync, faTerminal,
     // faAddressBook,
     faMarkdown,
 ];
