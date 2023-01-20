5e341d6af412fbb06e4b3454ae2459bac8c8cd18
razonyang <razonyang@gmail.com>
2023-01-20 12:20:48 +0800

    feat: allow showing multiple snackbar messages at the same time

--- assets/js/snackbar/index.ts
+++ assets/js/snackbar/index.ts
@@ -1,26 +1,21 @@
 class Snackbar {
-  element: HTMLElement;
+  container: HTMLElement;
 
   constructor() {
-    const e = document.createElement('div');
-    e.className =
-      'snackbar opacity-0 position-fixed w-100 d-flex justify-content-center';
-    document.body.appendChild(e);
-    this.element = e;
+    const container = document.createElement('div');
+    container.className = 'snackbar-container position-fixed d-flex justify-content-center align-items-center top-50 start-50 flex-column';
+    document.body.appendChild(container);
+    this.container = container;
   }
 
   show(body: string, duration = 2000) {
-    this.element.innerHTML =
-      '<div class="snackbar-body text-bg-secondary rounded px-3 py-2">' +
-      body +
-      '</div>';
-    this.element.classList.add('show');
+    const ele = document.createElement('div')
+    ele.className = 'snackbar text-bg-secondary rounded px-3 py-2 my-1'
+    ele.innerText = body
+    this.container.appendChild(ele)
     setTimeout(() => {
-      this.element.classList.remove('show');
-      setTimeout(() => {
-        this.element.innerHTML = '';
-      }, 1000)
-    }, duration);
+      ele.remove()
+    }, duration)
   }
 }
 
