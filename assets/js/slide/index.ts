8b7cfaf2473daf651d317ab3269f62e530b0ff92
razonyang <razonyang@gmail.com>
2023-01-29 01:01:43 +0800

    feat: add the slide component and use it on related posts

---
+++ assets/js/slide/index.ts
@@ -0,0 +1,26 @@
+(() => {
+    'use strict'
+
+    const scroll = (element) => {
+        const inner = element.parentElement.querySelector('.slide-inner')
+        const step = inner.offsetWidth
+        if (element.classList.contains('slide-control-left')) {
+            inner.scrollLeft -= step
+        } else {
+            inner.scrollLeft += step
+        }
+
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('.slide-control-left').forEach(element => {
+            element.addEventListener('click', () => {
+                scroll(element)
+            })
+        })
+        document.querySelectorAll('.slide-control-right').forEach(element => {
+            element.addEventListener('click', () => {
+                scroll(element)
+            })
+        })
+    })
+})()
