f8b09f2a585a3876fa8f896fecb348df71f8eafb
razonyang <razonyang@gmail.com>
2024-08-20 04:53:50 +0800

    fix: sidebar toggle on list and single page

--- assets/main/init/index.ts
+++ assets/main/init/index.ts
@@ -1,7 +1,7 @@
 import { default as LocalStorage } from "js/local-storage";
 
 (() => {
-  if (LocalStorage.getItem('sidebar-toggle') !== null) {
+  if (LocalStorage.getItem('sidebar-toggle') !== null && document.querySelector('main')?.getAttribute('data-kind') === 'page') {
     document.querySelector('main')?.classList.add('sidebar-none')
   }
 })()
