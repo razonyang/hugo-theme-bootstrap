936843974afd73652cddd55420e83b4c8a1d390c
razonyang <razonyang@gmail.com>
2024-08-19 12:13:12 +0800

    fix: code block maxlines not working all the time
    style: set max-height for code block to partially avoid CLS issue
    
    Fixes #1049

--- assets/js/code/index.ts
+++ assets/js/code/index.ts
@@ -3,10 +3,12 @@ import CodeBlock from "./code-block";
 
 class Code implements Component {
   run() {
-    document.querySelectorAll(".highlight").forEach((element: HTMLElement) => {
-      const block = new CodeBlock(element);
-      block.run();
-    });
+    document.addEventListener('DOMContentLoaded', () => {
+      document.querySelectorAll(".highlight").forEach((element: HTMLElement) => {
+        const block = new CodeBlock(element);
+        block.run();
+      });
+    })
   }
 }
 
